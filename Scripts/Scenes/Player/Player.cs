b22fc6e81069e9592c6edc4ce0c55c023226df9b
skathix <32309102+skathix@users.noreply.github.com>
2024-07-09 23:53:47 -0500

    development: Added jumping

--- Scripts/Scenes/Player/Player.cs
+++ Scripts/Scenes/Player/Player.cs
@@ -10,18 +10,48 @@ public partial class Player : CharacterBody2D
 	[Export]
 	private static float Gravity { get; set; } = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 	private AnimatedSprite2D sprite { get; set; }
-    private float velocityX { get; set; }
+	private Label debugLabel { get; set; }
+	private float velocityX { get; set; }
 	private float velocityY { get; set; }
 
-    public override void _Process(double delta)
+	[Export]
+	public int jumpForce { get; set; } = 300;
+	[Export]
+	public int maxJump { get; set; } = 1;
+	private int jumpCount { get; set; } = 0;
+
+	public override void _Process(double delta)
 	{
 		Movement(delta);
-		
+		Animation();
+		debugLabel.Text = $"JumpCount: {jumpCount}";
+	}
+
+	private void Animation()
+	{
+		if (Velocity.Y < 0)
+		{
+			sprite.Animation = "jump";
+		}
+		else if (Velocity.Y > 0)
+		{
+			sprite.Animation = "fall";
+		}
 	}
 
 	public override void _Ready()
 	{
 		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		debugLabel = GetNode<Label>("DebugLabel");
+		if (debugLabel == null)
+		{
+			GD.PrintErr("DebugLabel not found");
+		}
+		else
+		{
+			debugLabel.Text = "JumpCount: 0";
+			debugLabel.Visible = true;
+		}
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -35,7 +65,6 @@ public partial class Player : CharacterBody2D
 		velocityX = Velocity.X;
 		velocityY = Velocity.Y;
 
-		GD.Print(input);
 		if (input != 0)
 		{
 			if (input > 0)
@@ -58,13 +87,28 @@ public partial class Player : CharacterBody2D
 			velocityX = 0;
 			sprite.Animation = "idle";
 		}
+
+		if (IsOnFloor())
+		{
+			jumpCount = 0;
+		}
+
+		if (Input.IsActionJustPressed("jump") && maxJump > jumpCount)
+		{
+			velocityY = -jumpForce;
+			++jumpCount;
+		}
+
+
+
+
 		GravityForce(delta);
 
 		Velocity = new Vector2(velocityX, velocityY);
 		MoveAndSlide();
 	}
 
-	public void GravityForce(double delta) 
+	public void GravityForce(double delta)
 	{
 		velocityY += Gravity * (float)delta;
 	}
